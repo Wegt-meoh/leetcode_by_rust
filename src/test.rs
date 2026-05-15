41d435ac689284c1d13e12704ccca31c3ca0a788
wegt-meoh <wegt-meoh@outlook.com>
2026-05-15 17:34:01 +0800

    fix: test.rs causes error

--- src/test.rs
+++ src/test.rs
@@ -3,10 +3,5 @@ use std::collections::HashMap;
 pub struct Solution;
 
 impl Solution {
-    fn test() {
-        let a = HashMap::new();
-        if let Some(count) = a.get_mut(&1) {
-        } else {
-        }
-    }
+    fn test() {}
 }
