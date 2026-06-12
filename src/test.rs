004d77daed545a6386c210d799221540a7ea4798
wegt-meoh <wegt-meoh@outlook.com>
2026-06-12 09:20:14 +0800

    add: test.rs

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,11 @@
-use std::collections::HashMap;
-
 pub struct Solution;
 
 impl Solution {
-    fn test() {}
+    fn test() {
+        let a = String::from("sdfas");
+        for c in a.chars() {
+            let b = c as i32 - 'a' as i32;
+            println!("{b}");
+        }
+    }
 }
